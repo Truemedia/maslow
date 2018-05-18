84b7a3bf2ca9ca35786510b67e4eaace86b0cafb
Wade Penistone <wadespackages@yahoo.com>
2018-05-18 22:28:22 +0100

    Basic api for stage and needs contexts

--- index.js
+++ index.js
@@ -2,15 +2,60 @@ const pyramid = require('./pyramid.json');
 
 class Maslow
 {
-    constructor(level = null)
+    constructor(stage = null)
     {
-        this.level = level;
+        this.stage = stage;
         this.levels = pyramid;
     }
 
+    /**
+      * Get stages
+      * @return {array} All possible stages
+      */
     get stages()
     {
         return Object.keys(this.levels);
+    }
+
+    /**
+      * Get previous stage
+      * @return {string} Name of previous stage
+      */
+    get prevStage()
+    {
+        let nextIndex = this.stages.indexOf(this.stage);
+        nextIndex++;
+        return this.stages[nextIndex];
+    }
+
+    /**
+      * Get next stage
+      * @return {string} Name of next stage
+      */
+    get nextStage()
+    {
+        let prevIndex = this.stages.indexOf(this.stage);
+        prevIndex--;
+        return this.stages[prevIndex];
+    }
+
+    /**
+      * Get needs for stage
+      * @param {string} stage - Name of stage
+      * @return {array} Needs
+      */
+    needsForStage(stage)
+    {
+        return this.levels[stage];
+    }
+
+    /**
+      * Get current needs
+      * @return {array} Needs
+      */
+    get needs()
+    {
+        return this.needsForStage(this.stage);
     }
 }
 
--- test.js
+++ test.js
@@ -1,3 +1,9 @@
 const maslow = require('./index');
 
-console.log(new maslow().stages);
+// Make a person with most basic needs
+let person = new maslow('physiological');
+console.log(`The pyramid of needs has the following stages: ${person.stages}`);
+console.log(`Person is at the stage: ${person.stage}`);
+console.log(`Previous stage is: ${person.prevStage}`);
+console.log(`Next stage is ${person.nextStage}`);
+console.log(`Person needs: ${person.needs}`);
