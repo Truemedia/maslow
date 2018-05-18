562da82b82b182f8d379abb39c76609222111264
Wade Penistone <wadespackages@yahoo.com>
2018-05-18 22:08:59 +0100

    Pyramid now in json file

--- index.js
+++ index.js
@@ -1,37 +1,11 @@
+const pyramid = require('./pyramid.json');
+
 class Maslow
 {
     constructor(level = null)
     {
         this.level = level;
-        this.levels = { // The pyramid
-            "self-actualization": [
-                "accomplishment",
-                "long-term-gratification"
-            ],
-            "esteem": [
-                "self-respect",
-                "self-esteem",
-                "short-term-gratification"
-            ],
-            "belonging": [
-                "friendships",
-                "intimacy",
-                "family"
-            ],
-            "safety": [
-                "personal-security",
-                "financial-security",
-                "health",
-                "well-being"
-            ],
-            "physiological": [
-                "air",
-                "water",
-                "food",
-                "shelter",
-                "sleep"
-            ]
-        };
+        this.levels = pyramid;
     }
 
     get stages()
